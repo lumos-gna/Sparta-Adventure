f6fcc7af4c4ad58980458d4a8792570d22efd092
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-05-23 11:25:11 +0900

    feat : 네비 가중치 추가

--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -1,33 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class NPC : MonoBehaviour
 {
-    [SerializeField] private float wanderRadius = 10f;
-    [SerializeField] private float wanderInterval = 3f;
-
     private NavMeshAgent _agent;
-    private float _timer;
+    
+    private float _wanderRadius = 10f;
+    private float _interval = 5f;
+
+    private float _timer = 0;
+
     private void Awake()
-    {   
+    {
         _agent = GetComponent<NavMeshAgent>();
-        _timer = wanderInterval;
+    }
+
+    private void Start()
+    {
+        _timer = _interval;
     }
 
     private void Update()
     {
         _timer += Time.deltaTime;
-
-        if (_timer >= wanderInterval)
+        
+        if (_timer >= _interval)
         {
-            Vector3 newPos;
-            if (GetRandomPoint(transform.position, wanderRadius, out newPos))
+            if (GetRandomPoint(transform.position, _wanderRadius, out Vector3 destination))
             {
-                _agent.SetDestination(newPos);
+                NavMeshPath path = new NavMeshPath();
+                if (_agent.CalculatePath(destination, path) && path.status == NavMeshPathStatus.PathComplete)
+                {
+                    _agent.SetPath(path);
+                }
             }
-
             _timer = 0f;
         }
     }
@@ -36,16 +45,15 @@ public class NPC : MonoBehaviour
     {
         for (int i = 0; i < 30; i++)
         {
-            Vector3 randomPoint = center + Random.insideUnitSphere * range;
-            randomPoint.y = center.y;
+            Vector3 randomPos = center + Random.insideUnitSphere * range;
+            randomPos.y = center.y;
 
-            if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, 1.0f, NavMesh.AllAreas))
+            if (NavMesh.SamplePosition(randomPos, out NavMeshHit hit, 1.0f, NavMesh.AllAreas))
             {
                 result = hit.position;
                 return true;
             }
         }
-
         result = center;
         return false;
     }
